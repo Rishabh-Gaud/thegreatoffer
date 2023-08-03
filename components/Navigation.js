04dffd17badbd11d9bfad586bcc8029c7326ad6a
Rishabh Kumar Gaud <rishabh.kumbh@gmail.com>
2023-08-03 16:32:32 +0530

    next project again initialise

---
+++ components/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div
+      className=" bg-red-600
+  
+  
+  text-yellow-500"
+    >
+      Navigation
+    </div>
+  );
+};
+
+export default Navigation;
