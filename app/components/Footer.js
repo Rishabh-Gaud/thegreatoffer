beddb03453d59b1ce049740615607cac4cc51ce2
kapil <kapilchaudhary8280@gmail.com>
2023-08-07 19:11:13 +0530

    v0 initial

---
+++ app/components/Footer.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import logo from "../images/logo.png";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-20 bg-sky-500 text-gray-300 flex items-center justify-center gap-4">
+      <Image className="w-24" src={logo} alt="logo" />
+      <p className="text-sm -mt-4">
+        All rights reserved{" "}
+        <a
+          className="hover:text-white hover:underline decoration-[1px] cursor-pointer duration-300"
+          href="https://reactbd.com"
+          target="_blank"
+        >
+          @thegreatoffer.com
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
